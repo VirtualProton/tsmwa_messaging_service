15fba5ac578d559da20b41491e4289118b702fae
Pritham <impritham@gmail.com>
2025-11-30 12:12:34 +0530

    invoice tracker

--- src/controller/log.controller.ts
+++ src/controller/log.controller.ts
@@ -12,6 +12,7 @@ export const getLogs = async (req: Request, res: Response, next: NextFunction) =
         } else {
             logger.error(`❌ Error in meeting_schedule controller: ${JSON.stringify(err)}`);
         }
+        
 
         return res.status(400).json({
             success: false,
